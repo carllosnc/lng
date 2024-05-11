2d9c1519deef04504e92f8f85a13102ec057f456
Carlos Costa <carllos.nc@gmail.com>
2024-05-11 01:24:18 -0300

    Adding comments

--- lib/lng_notifier.dart
+++ lib/lng_notifier.dart
@@ -5,21 +5,29 @@ class _LngNotifier extends ChangeNotifier {
   String currentLanguage = 'en';
   Map<String, Map<String, dynamic>> labels = {};
 
+  /// set the current language
+  /// [language] is the language code
   void setLanguage(String language) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
     currentLanguage = language.toString();
 
+    /// set the current language to the shared preferences
     prefs.setString('lang', language.toString());
 
+    /// notify the listeners
     notifyListeners();
   }
 
+  /// add a new language
+  /// [lngLabels] is a map with the language code as key and the labels as value
   void addLabels(Map<String, Map<String, dynamic>> lngLabels) {
     labels = lngLabels;
     notifyListeners();
   }
 
+  /// get a label from the current language
+  /// [key] is the key of the label
   dynamic get(String key) {
     return labels[currentLanguage]![key];
   }
--- lib/lng_page.dart
+++ lib/lng_page.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 import './lng_notifier.dart';
 
+/// Opens a modal bottom sheet to select a language.
+/// The modal bottom sheet is closed when the user selects a language.
 openLng({
+  ///[context] The context to open the modal bottom sheet in.
   required BuildContext context,
+
+  ///[title] The title of the modal bottom sheet.
   String title = 'Set Language',
+
+  ///[titleStyle] The style of the title of the modal bottom sheet.
+  TextStyle? titleStyle = const TextStyle(
+    fontSize: 18,
+  ),
+
+  ///[backgroundColor] The background color of the modal bottom sheet.
+  Color? backgroundColor,
 }) {
   showModalBottomSheet(
     isScrollControlled: true,
     context: context,
+    backgroundColor: backgroundColor,
     builder: (context) {
-      return SizedBox(
+      return Container(
+        key: const Key('lng_container'),
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+        ),
         height: MediaQuery.of(context).size.height - 100,
         child: Scaffold(
+          key: const Key('lng_scaffold'),
+          backgroundColor: Colors.transparent,
           appBar: AppBar(
-            title: Text(title),
+            key: const Key('lng_appbar'),
+            backgroundColor: Colors.transparent,
+            title: Text(
+              title,
+              style: titleStyle,
+            ),
             leading: IconButton(
+              key: const Key('lng_appbar_close'),
               icon: const Icon(Icons.close),
               onPressed: () {
                 Navigator.of(context).pop();
@@ -22,10 +49,14 @@ openLng({
             ),
           ),
           body: ListView.builder(
+            key: const Key('lng_listview'),
             itemCount: lng.labels.length,
             itemBuilder: (context, index) {
               return ListTile(
-                title: Text(lng.get(lng.labels.keys.toList()[index])),
+                key: Key('lng_listview_item_${lng.labels.keys.toList()[index]}'),
+                title: Text(
+                  lng.get(lng.labels.keys.toList()[index]),
+                ),
                 onTap: () {
                   lng.setLanguage(
                     lng.labels.keys.toList()[index],
