968e4ed916f74115d7ddac52047845d502c47e0c
Carlos Costa <carllos.nc@gmail.com>
2024-05-15 13:20:05 -0300

    Moving labels to another file

---
+++ example/lib/labels.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+Map<String, Map<String, dynamic>> labels = {
+  // English
+  'en': {
+    'pt': 'Portuguese',
+    'en': 'English',
+    'es': 'Spanish',
+    'fr': 'French',
+    'text':
+        "The USA is a country of 50 states that cover a vast swath of North America, with Alaska in the northwest and Hawaii in the Pacific Ocean extending the country's presence. The main cities on the Atlantic coast are New York, a global financial and cultural center, and the capital, Washington, DC. Chicago, a Midwestern metropolis, is known for its important architecture, while Los Angeles, on the West Coast, is famous for Hollywood film productions.",
+    'title': 'United States',
+    'color': Colors.red,
+    'image': 'assets/usa.png',
+    'set_lang': 'Set language',
+  },
+  // Spanish
+  'es': {
+    'pt': 'Portugués',
+    'en': 'Inglés',
+    'es': 'Español',
+    'fr': 'Francés',
+    'text':
+        'España, un país europeo de la península ibérica, tiene 17 regiones autónomas con diversa geografía y cultura. Madrid, la capital, alberga el Palacio Real y el Museo del Prado, obras construidas por maestros europeos. Segovia tiene un castillo medieval (Alcázar) y un acueducto romano intacto. La capital de Cataluña, Barcelona, ​​cuenta con monumentos modernistas de Antoni Gaudí, como la Iglesia de la Sagrada Familia.',
+    'title': 'España',
+    'color': Colors.orange.shade900,
+    'image': 'assets/spain.png',
+    'set_lang': 'Definir idioma',
+  },
+  // French
+  'fr': {
+    'pt': 'Portugais',
+    'en': 'Anglais',
+    'es': 'Espagnol',
+    'fr': 'Français',
+    'text':
+        "La France, en Europe occidentale, possède des villes médiévales, des villages alpins et des plages méditerranéennes. Paris, sa capitale, est célèbre pour ses maisons de haute couture, ses musées d'art classique comme le Louvre et ses monuments comme la Tour Eiffel. Le pays est également connu pour ses vins et sa cuisine sophistiquée. Des dessins anciens de la grotte de Lascaux, du Théâtre romain de Lyon et du vaste château de Versailles témoignent de sa riche histoire.",
+    'title': 'France',
+    'color': Colors.blue,
+    'image': 'assets/france.png',
+    'set_lang': 'Définir la langue',
+  },
+  // Portuguese
+  'pt': {
+    'pt': 'Português',
+    'en': 'Inglês',
+    'es': 'Espanhol',
+    'fr': 'Francês',
+    'text':
+        'O Brasil, um vasto país sul-americano, estende-se da Bacia Amazônica, no norte, até os vinhedos e as gigantescas Cataratas do Iguaçu, no sul. O Rio de Janeiro, simbolizado pela sua estátua de 38 metros de altura do Cristo Redentor, situada no topo do Corcovado, é famoso pelas movimentadas praias de Copacabana e Ipanema, bem como pelo imenso e animado Carnaval, com desfiles de carros alegóricos, fantasias extravagantes e samba.',
+    'title': 'Brasil',
+    'color': Colors.green,
+    'image': 'assets/brazil.png',
+    'set_lang': 'Mudar de idioma',
+  }
+};
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,61 +1,13 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
 import 'package:lng/lng.dart';
+import './labels.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   lngCore.init(defaultLang: 'en');
-  lng.addLabels({
-    'en': {
-      'pt': 'Portuguese',
-      'en': 'English',
-      'es': 'Spanish',
-      'fr': 'French',
-      'text':
-          "The USA is a country of 50 states that cover a vast swath of North America, with Alaska in the northwest and Hawaii in the Pacific Ocean extending the country's presence. The main cities on the Atlantic coast are New York, a global financial and cultural center, and the capital, Washington, DC. Chicago, a Midwestern metropolis, is known for its important architecture, while Los Angeles, on the West Coast, is famous for Hollywood film productions.",
-      'title': 'United States',
-      'color': Colors.red,
-      'image': 'assets/usa.png',
-      'set_lang': 'Set language',
-    },
-    'es': {
-      'pt': 'Portugués',
-      'en': 'Inglés',
-      'es': 'Español',
-      'fr': 'Francés',
-      'text':
-          'España, un país europeo de la península ibérica, tiene 17 regiones autónomas con diversa geografía y cultura. Madrid, la capital, alberga el Palacio Real y el Museo del Prado, obras construidas por maestros europeos. Segovia tiene un castillo medieval (Alcázar) y un acueducto romano intacto. La capital de Cataluña, Barcelona, ​​cuenta con monumentos modernistas de Antoni Gaudí, como la Iglesia de la Sagrada Familia.',
-      'title': 'España',
-      'color': Colors.orange.shade900,
-      'image': 'assets/spain.png',
-      'set_lang': 'Definir idioma',
-    },
-    'fr': {
-      'pt': 'Portugais',
-      'en': 'Anglais',
-      'es': 'Espagnol',
-      'fr': 'Français',
-      'text':
-          "La France, en Europe occidentale, possède des villes médiévales, des villages alpins et des plages méditerranéennes. Paris, sa capitale, est célèbre pour ses maisons de haute couture, ses musées d'art classique comme le Louvre et ses monuments comme la Tour Eiffel. Le pays est également connu pour ses vins et sa cuisine sophistiquée. Des dessins anciens de la grotte de Lascaux, du Théâtre romain de Lyon et du vaste château de Versailles témoignent de sa riche histoire.",
-      'title': 'France',
-      'color': Colors.blue,
-      'image': 'assets/france.png',
-      'set_lang': 'Définir la langue',
-    },
-    'pt': {
-      'pt': 'Português',
-      'en': 'Inglês',
-      'es': 'Espanhol',
-      'fr': 'Francês',
-      'text':
-          'O Brasil, um vasto país sul-americano, estende-se da Bacia Amazônica, no norte, até os vinhedos e as gigantescas Cataratas do Iguaçu, no sul. O Rio de Janeiro, simbolizado pela sua estátua de 38 metros de altura do Cristo Redentor, situada no topo do Corcovado, é famoso pelas movimentadas praias de Copacabana e Ipanema, bem como pelo imenso e animado Carnaval, com desfiles de carros alegóricos, fantasias extravagantes e samba.',
-      'title': 'Brasil',
-      'color': Colors.green,
-      'image': 'assets/brazil.png',
-      'set_lang': 'Mudar de idioma',
-    }
-  });
+  lng.addLabels(labels);
 
   runApp(const App());
 }
