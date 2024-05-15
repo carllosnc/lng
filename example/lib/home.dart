87d2456a6145265adc85c203c3eb77e669160674
Carlos Costa <carllos.nc@gmail.com>
2024-05-15 01:11:27 -0300

    Update the main example

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -16,26 +16,64 @@ class _HomeState extends State<Home> with LngMixin {
     return Scaffold(
       backgroundColor: lng.get('color'),
       appBar: AppBar(
-        title: Text(lng.get('app_title')),
+        title: const Text("lng"),
         actions: [
           IconButton(
             icon: const Icon(Icons.translate),
             onPressed: () {
               openLng(
                 context: context,
-                title: "Set language",
+                title: lng.get('set_lang'),
               );
             },
           ),
         ],
       ),
-      body: SizedBox(
+      body: Container(
+        padding: const EdgeInsets.all(40),
         width: double.infinity,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(lng.get('hello')),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(200),
+              ),
+              padding: const EdgeInsets.all(5),
+              child: Image.asset(
+                lng.get('image'),
+                width: 160,
+                height: 160,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Text(
+                lng.get('title'),
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              lng.get('text'),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
           ],
         ),
       ),
