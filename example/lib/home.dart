f280a1b9868056f08e244e204f1a64e6825ad6ad
Carlos Costa <carllos.nc@gmail.com>
2024-05-21 15:26:31 -0300

    Update example

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -24,6 +24,7 @@ class _HomeState extends State<Home> with LngMixin {
               openLng(
                 context: context,
                 title: lng.get('set_lang'),
+                borderRadius: 25,
               );
             },
           ),
