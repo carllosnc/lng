d7dcac89600d3a52758c042b92735b5b93f9578e
Carlos Costa <carllos.nc@gmail.com>
2024-06-25 07:32:40 -0300

    Adding new test cases

--- test/lng_test.dart
+++ test/lng_test.dart
@@ -1,1 +1,46 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lng/lng.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  group("lng", () {
+    setUp(() {
+      SharedPreferences.setMockInitialValues({});
+    });
+
+    test("Check lng.init", () {
+      lng.init(defaultLang: 'en');
+      expect(lng.currentLanguage, 'en');
+    });
+
+    test("Check lng.addLabels", () {
+      lng.addLabels({
+        'en': {
+          'es': 'Spanish',
+          'en': 'English',
+          'text': 'Hello World',
+        },
+        'es': {
+          'es': 'Español',
+          'en': 'Inglés',
+          'text': 'Hola Mundo',
+        },
+      });
+
+      expect(lng.get('es'), 'Spanish');
+      expect(lng.get('en'), 'English');
+      expect(lng.get('text'), 'Hello World');
+    });
+
+    test("Check lng.setLanguage", () {
+      lng.setLanguage('es');
+      lng.onLangChange = (String language) {
+        expect(language, 'es');
+
+        expect(lng.get('es'), 'Español');
+        expect(lng.get('en'), 'Inglés');
+        expect(lng.get('text'), 'Hola Mundo');
+      };
+    });
+  });
+}
